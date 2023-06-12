db1c9962bc980ef37e650f3deb00e3b6d06a8224
kshitij-hash <83551355+kshitij-hash@users.noreply.github.com>
2023-06-13 00:54:46 +0530

    seeds changes
    connecting to mongodb atlas to pre-populate the database

--- seeds/index.js
+++ seeds/index.js
@@ -3,11 +3,12 @@ const cities = require('./cities');
 const {places, descriptors} = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
+mongoose.connect(dbUrl);
 
 main().catch(err => console.log("Connection error: ",err));
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+  await mongoose.connect(dbUrl);
   console.log("Database connected")
 }
 
@@ -33,8 +34,8 @@ const seedDB = async() => {
             },
             images : [
                 {
-                  url: 'https://res.cloudinary.com/doyuvkbt5/image/upload/v1686478720/YelpCamp/alex-fWD7KqlsCwE-unsplash_1_kd3az4.jpg',
-                  filename: 'YelpCamp/alex-fWD7KqlsCwE-unsplash_1_kd3az4'
+                  url: 'https://res.cloudinary.com/doyuvkbt5/image/upload/v1686597758/YelpCamp/jordan-pulmano-PoAkzayxhYE-unsplash_e9zin8.jpg',
+                  filename: 'YelpCamp/jordan-pulmano-PoAkzayxhYE-unsplash_e9zin8'
                 }
             ]
         })
